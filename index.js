85c2eeeed9a7b02f27c40d23c98b0d3405b51de7
Flyu <2593685269@qq.com>
2022-08-23 16:31:34 +0800

    第一次提交

---
+++ index.js
@@ -0,0 +1,11 @@
+function throttled(fn, delay=500) {
+  let timer = null
+  return (...args) => {
+    if(!timer){
+      timer = setTimeout(() => {
+        console.log(this)
+        fn.apply(this,args)
+      },delay);
+    }
+  }
+}
